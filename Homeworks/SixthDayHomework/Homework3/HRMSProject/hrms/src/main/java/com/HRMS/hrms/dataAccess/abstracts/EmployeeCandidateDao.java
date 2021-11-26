82fa06da9785c5a9ddd39d6ab2b060ff54aa25bf
Mehmet Hanifi Aydın <86236897+hnfaydn@users.noreply.github.com>
2021-11-27 00:25:09 +0300

    Update EmployeeCandidateDao.java

--- Homeworks/SixthDayHomework/Homework3/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/abstracts/EmployeeCandidateDao.java
+++ Homeworks/SixthDayHomework/Homework3/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/abstracts/EmployeeCandidateDao.java
@@ -3,7 +3,7 @@ package com.HRMS.hrms.dataAccess.abstracts;
 import com.HRMS.hrms.entities.concretes.EmployeeCandidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+@Repository
 public interface EmployeeCandidateDao extends JpaRepository<EmployeeCandidate, Integer> {
 
 
