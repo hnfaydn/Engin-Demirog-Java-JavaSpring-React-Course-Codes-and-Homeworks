dd3ecb9388d0353331e751a00cc8efca960d648a
Mehmet Hanifi Aydın <86236897+hnfaydn@users.noreply.github.com>
2021-11-27 00:26:12 +0300

    Update UserDao.java

--- Homeworks/SixthDayHomework/Homework3/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/abstracts/UserDao.java
+++ Homeworks/SixthDayHomework/Homework3/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/abstracts/UserDao.java
@@ -3,5 +3,6 @@ package com.HRMS.hrms.dataAccess.abstracts;
 import com.HRMS.hrms.entities.abstracts.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserDao extends JpaRepository<User, Integer> {
 }
