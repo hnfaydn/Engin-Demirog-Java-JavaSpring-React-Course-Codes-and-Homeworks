909fc9539e06924d7327bfcd05516b14987c8917
Mehmet Hanifi Aydın <86236897+hnfaydn@users.noreply.github.com>
2021-11-27 00:25:44 +0300

    Update JobTitleDao.java

--- Homeworks/SixthDayHomework/Homework3/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/abstracts/JobTitleDao.java
+++ Homeworks/SixthDayHomework/Homework3/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/abstracts/JobTitleDao.java
@@ -3,6 +3,7 @@ package com.HRMS.hrms.dataAccess.abstracts;
 import com.HRMS.hrms.entities.concretes.JobTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface JobTitleDao extends JpaRepository<JobTitle, Integer> {
 
 }
