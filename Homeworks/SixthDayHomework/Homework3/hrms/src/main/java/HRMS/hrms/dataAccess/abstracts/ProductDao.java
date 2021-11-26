572c71224c4462bb6adc968f6b0ccea6de424d49
Mehmet Hanifi Aydın <86236897+hnfaydn@users.noreply.github.com>
2021-11-27 00:15:57 +0300

    Delete Homeworks/SixthDayHomework/Homework3/hrms directory

--- Homeworks/SixthDayHomework/Homework3/hrms/src/main/java/HRMS/hrms/dataAccess/abstracts/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package HRMS.hrms.dataAccess.abstracts;
-
-import HRMS.hrms.entities.concretes.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductDao extends JpaRepository<Users,Integer> {
-}
