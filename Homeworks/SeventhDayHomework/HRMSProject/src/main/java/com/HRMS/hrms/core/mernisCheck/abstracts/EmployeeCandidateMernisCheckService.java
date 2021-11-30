c0b2965bf5b4cb3fb382e7399a30a8b2614925f8
Mehmet Hanifi Aydın <86236897+hnfaydn@users.noreply.github.com>
2021-12-01 01:46:32 +0300

    Delete Homeworks/SeventhDayHomework/HRMSProject/src directory

--- Homeworks/SeventhDayHomework/HRMSProject/src/main/java/com/HRMS/hrms/core/mernisCheck/abstracts/EmployeeCandidateMernisCheckService.java
+++
@@ -1,9 +0,0 @@
-package com.HRMS.hrms.core.mernisCheck.abstracts;
-
-import com.HRMS.hrms.core.results.Result;
-import com.HRMS.hrms.entities.userEntities.concretes.EmployeeCandidate;
-
-public interface EmployeeCandidateMernisCheckService {
-
-    Result employeeCandidateMernisCheck(EmployeeCandidate employeeCandidate) throws Exception;
-}
