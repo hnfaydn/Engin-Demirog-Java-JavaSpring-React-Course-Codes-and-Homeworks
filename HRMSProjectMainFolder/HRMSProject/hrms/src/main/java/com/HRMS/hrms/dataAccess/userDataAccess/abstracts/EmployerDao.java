8ea7738c2a2ed2b19b246c312e2f448dfe2ebd2b
Mehmet Hanifi Aydın <86236897+hnfaydn@users.noreply.github.com>
2021-12-01 01:54:03 +0300

    Delete HRMSProjectMainFolder/HRMSProject directory

--- HRMSProjectMainFolder/HRMSProject/hrms/src/main/java/com/HRMS/hrms/dataAccess/userDataAccess/abstracts/EmployerDao.java
+++
@@ -1,9 +0,0 @@
-package com.HRMS.hrms.dataAccess.userDataAccess.abstracts;
-
-import com.HRMS.hrms.entities.userEntities.concretes.Employer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployerDao extends JpaRepository<Employer, Integer> {
-}
